0d8394b6baf82a1a8e9b3a5b7b505ed6b37a6cd3
milemik <milemik68@gmail.com>
2022-01-22 20:07:13 +0100

    Added -d option to delete all pre-push-git files

--- pre-push-script.sh
+++ pre-push-script.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
-echo "Creating pre push"
 
 gitdir=".git"
 setting_file="pre-push-commands.ini"
 push_file="pre-push-local"
 commit_file="pre-commit-local"
 
-git_push_sample=".git/hooks/pre-push.sample"
-git_commit_sample=".git/hooks/pre-commit.sample"
 git_hooks_dir=".git/hooks/"
+
+# Files that git uses for pre-commit and pre-push
+git_pre_commit_file = "$git_hooks_dir/pre-commit"
+git_pre_push_file = "$git_hooks_dir/pre-push"
 
 # COMMANDS FOR CHECK WHICH COMMAND GOES TO WHICH FILE
 # based on the .ini file with commands
 commit_line="[pre-commit]"
 push_line="[pre-push]"
 check=0
+
+
+# DELETE ALL PRE-PUSH-GIT FILES EVEN THIS FILE!
+if [[ $1 == "-d" ]]
+then
+	echo "Removing pre-push-git files"
+	if [[ -f $git_pre_commit_file ]]
+	then
+		echo "Removing $git_pre_commit_file"
+		rm $git_pre_commit_file
+	fi
+	
+	if [[ -f $git_pre_push_file ]]
+	then
+		echo "Removing $git_pre_push_file"
+		rm $git_pre_push_file
+	fi
+
+	if [[ -f $setting_file ]]
+	then
+		echo "Removing $setting_file"
+		rm $setting_file
+	if
+
+	rm "pre-test.sh"
+	echo "All files removed"
+	exit
+fi
+
+echo "Creating pre push"
 
 if [[ -f $setting_file ]]
 then
@@ -63,8 +94,8 @@ then
 	echo "# End of your custom commands" >> $push_file
 	echo "# ==================================================================" >> $push_file
 	echo "Copy file to $git_hooks_dir"
-	cp $push_file "$git_hooks_dir/pre-push"
-	chmod +x "$git_hooks_dir/pre-push"
+	cp $push_file $git_pre_push_file
+	chmod +x $git_pre_push_file
 	rm $push_file
 
 	# SET PRE-COMMIT
@@ -95,8 +126,8 @@ then
 	echo "# End of your custom commands" >> $commit_file
 	echo "# ==================================================================" >> $commit_file
 	echo "Copy file to $git_hooks_dir"
-	cp $commit_file "$git_hooks_dir/pre-commit"
-	chmod +x "$git_hooks_dir/pre-commit"
+	cp $commit_file $git_pre_commit_file
+	chmod +x $git_pre_commit_file
 	rm $commit_file
 else
 	echo "Git dir found $gitdir"
